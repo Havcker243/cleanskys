99cb0b260b2b89ab0d580d23ad44fb53d15e5e30
Oludolapo Adegbesan <oadegbesan25@my.whitworth.edu>
2024-07-06 23:45:18 -0700

    did some commenting

--- weather-extension-frontend/src/Weather.js
+++ weather-extension-frontend/src/Weather.js
@@ -1,18 +1,23 @@
 // Importing necessary React hooks and CSS.
-import { Link } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import "./App.css"; // Make sure you import your styleshee
 // Defining the Weather component.
 function Weather() {
+  // State for storing weather data fetched from the API.
   const [weatherData, setWeatherData] = useState(null);
+    // State to store the CSS class based on weather conditions.\
   const [weatherClass, setWeatherClass] = useState(""); // Add a state to hold the class name
+    // State to toggle between Celsius and Fahrenheit.
   const [isCelsius, setIsCelsius] = useState(true); // True for Celsius, false for Fahrenheit
+    // State to store weather-related messages.
   const [weatherMessage, setWeatherMessage] = useState("");
+    // State to store temperature-related image and description.
   const [temperatureInfo, setTemperatureInfo] = useState({
     image: "",
     description: "",
   });
 
+    // Array of objects defining temperature ranges and corresponding imagery/descriptions.
   const temperatureRanges = [
     { max: -10, image: "ice.png", description: "Extremely Cold" },
     { min: -10, max: 0, image: "snow.png", description: "Freezing" }, // Changed to 'snow.png' which might be more visually appropriate for freezing conditions.
@@ -53,7 +58,8 @@ function Weather() {
     },
   ];
 
-  // Function to get the user temperature
+
+  // Function to determine the temperature range based on current temperature.
   function getTemperatureRange(temp) {
     for (const range of temperatureRanges) {
       if (
@@ -66,16 +72,19 @@ function Weather() {
     return { image: "default.png", description: "Temperature out of range" }; // Default case if no range matches
   }
 
+  // Function to convert temperature from Celsius to Fahrenheit.
   function convertToFahrenheit(celsius) {
     return (celsius * 9) / 5 + 32;
   }
 
+    // Function to utilize the Web Speech API to speak weather information.
   const speak = (text) => {
     const synth = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(text);
     synth.speak(utterance);
   };
 
+    // Handler for triggering speech of current weather conditions.
   const handleSpeak = () => {
     if (!weatherData) return;
     const tempUnit = isCelsius ? "degrees Celsius" : "degrees Fahrenheit";
@@ -95,6 +104,7 @@ function Weather() {
     speak(text);
   };
 
+  // Effect hook to fetch weather data once component is mounted.
   useEffect(() => {
     if (weatherData && weatherData.main && weatherData.main.temp) {
       const tempInfo = getTemperatureRange(weatherData.main.temp);
@@ -118,7 +128,7 @@ function Weather() {
         // Logging any errors that occur during the fetch operation.
         console.error("Error fetching the weather data:", error);
       }
-    };
+      };
     // Function to get user's location
     const getLocation = () => {
       if (navigator.geolocation) {
@@ -180,12 +190,16 @@ function Weather() {
   const calculationTime = weatherData.timezone
     ? getLocalTime(weatherData.dt, weatherData.timezone)
     : "";
-
+// Function used to convert the timestamp to dates 
   function convertTimestampToDate(unixTimestamp) {
     const date = new Date(unixTimestamp * 1000); // Convert the timestamp from seconds to milliseconds
     return date.toLocaleDateString("en-US"); // Returns the date in the format "MM/DD/YYYY" for the US locale
   }
+
+    // Convert UNIX timestamp to a human-readable date.
 const finaldate = convertTimestampToDate(weatherData.dt)
+
+  // Main JSX for displaying weather information.
   return (
     <div className={`weather ${weatherClass}`}>
       {weatherData.main ? (
